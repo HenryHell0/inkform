4637a5a716e0106caa0f4c8cf296b76df93f13bc
Henry Holton <noway1526@gmail.com>
2026-03-02 14:54:42 -0500

    fix widget style bug

--- frontend/src/composables/useWidgetStyles.ts
+++ frontend/src/composables/useWidgetStyles.ts
@@ -4,9 +4,8 @@ import type { Widget } from '@/utils/widgetData'
 export function useWidgetStyles(widget: Widget) {
 	return computed(() => {
 		return {
-			// left: `${widget.x}px`,
-			// top: `${widget.y}px`,
-			transform: `translate(${widget.x}px, ${widget.y}px)`, // apparently this is better performance says chat
+			left: `${widget.x}px`,
+			top: `${widget.y}px`,
 			width: `${widget.width}px`,
 			height: `${widget.height}px`,
 			zIndex: widget.zIndex,
