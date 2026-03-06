0ba590bf3535db854fcb4f40e711948d84727328
Henry Holton <noway1526@gmail.com>
2026-03-06 14:37:42 -0500

    implement FullscreenDialog.vue to make creating fullscreen dialogs easier in the future Work on #6

--- frontend/src/components/dialogs/FeedbackDialog.vue
+++ frontend/src/components/dialogs/FeedbackDialog.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useDialogStore } from '@/stores/useDialogStore'
 import { reactive } from 'vue'
+import FullscreenDialog from './templates/FullscreenDialog.vue'
 const dialogStore = useDialogStore()
 const API_BASE = import.meta.env.VITE_API_BASE
 
@@ -27,52 +28,17 @@ function handleSubmit() {
 }
 </script>
 <template>
-	<div class="backdrop" @click="dialogStore.closeDialog('feedback')">
-		<div class="container" @click.stop>
-			<h1>Give Feedback!</h1>
+	<FullscreenDialog :close="() => dialogStore.closeDialog('feedback')">
+		<h1>Give Feedback!</h1>
 
-			<div class="form">
-				<input v-model="data.userInfo" type="text" placeholder="Name or Email" />
-				<textarea
-					v-model="data.message"
-					class="message"
-					type="text"
-					placeholder="Your feedback"
-				></textarea>
-				<button @click="handleSubmit"><b>Send</b></button>
-			</div>
+		<div class="form">
+			<input v-model="data.userInfo" type="text" placeholder="Name or Email" />
+			<textarea v-model="data.message" class="message" type="text" placeholder="Your feedback"></textarea>
+			<button @click="handleSubmit"><b>Send</b></button>
 		</div>
-	</div>
+	</FullscreenDialog>
 </template>
 <style>
-.backdrop {
-	z-index: 1000;
-
-	position: absolute;
-	left: 0px;
-	top: 0px;
-	width: 100%;
-	height: 100%;
-	display: flex;
-
-	justify-content: center;
-	align-items: center;
-
-	backdrop-filter: blur(3px);
-	background-color: rgba(177, 177, 177, 0.5);
-}
-
-.container {
-	width: 400px;
-	min-width: 10em;
-	border: 2px solid var(--color-zinc-500);
-	border-radius: 5px;
-	background: var(--color-bg-0);
-
-	text-align: center;
-	padding: 20px;
-}
-
 .form {
 	display: flex;
 	flex-direction: column;
