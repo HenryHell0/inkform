11ab0608fe7f4e4359c4d620c9081b93a88f6a38
Henry Holton <noway1526@gmail.com>
2026-03-05 19:24:46 -0500

    implement DialogHost

---
+++ frontend/src/components/core/DialogHost.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useDialogStore } from '@/stores/useDialogStore'
+import { loadDialogs } from '../dialogs/registerDialogs'
+import { onMounted } from 'vue'
+
+loadDialogs()
+const dialogStore = useDialogStore()
+</script>
+<template>
+	<!-- issue with dialog stacking in the future FYI -->
+	<component
+		v-for="dialog in dialogStore.openDialogs"
+		:key="dialog.name"
+		:is="dialogStore.dialogRegistry[dialog.name]"
+		v-bind="dialog.props"
+	/>
+</template>
