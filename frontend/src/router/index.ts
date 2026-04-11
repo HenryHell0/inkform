086b8ae1d777e67955dceaf2e97c62f36e5f3a60
Henry Holton <noway1526@gmail.com>
2026-04-11 18:51:56 -0400

    add `src/routes/index.ts` to manage routes/options

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+// todo add features dialog too
+import InfoDialog from '@/views/dialogs/InfoDialog.vue'
+import FeedbackDialog from '@/views/dialogs/FeedbackDialog.vue'
+import Whiteboard from '@/views/Whiteboard.vue'
+
+// alright so the way this works is the whole app is `App.vue` with a <RouteView> but we can make different routes now!
+// rn the main one is / pointing to Whiteboard.vue, and then it has /info and /feedback, so it uses it's own <RouteView> component for children
+// but, we could add /store, /info seperately, etc!, and it just swaps out <RouteView> for that component!
+
+const routes: RouteRecordRaw[] = [
+	{
+		path: '/',
+		component: Whiteboard,
+		children: [
+			{
+				path: '',
+				name: 'home', // these need to be GLOABALLY SPECIFIC! (once we have multiple pages -> "whiteboard:home")
+				component: { template: '<div />' }, // hacky empty component
+			},
+			{
+				path: 'info',
+				name: 'info',
+				component: InfoDialog,
+			},
+			{
+				path: 'feedback',
+				name: 'feedback',
+				component: FeedbackDialog,
+			},
+		],
+	},
+
+	// future other paths!
+	// {
+	// 	path: '/store',
+	// 	component: () => import('@/views/StorePage.vue'),
+	// },
+]
+
+export const router = createRouter({
+	history: createWebHistory(),
+	routes,
+})
