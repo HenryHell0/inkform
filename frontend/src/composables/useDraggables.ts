b89450525a942c5af933451efb8c16a4e44eabd3
Henry Holton <noway1526@gmail.com>
2026-04-16 14:49:27 -0400

    change `useWidgetDrag` and `useWidgetResize` to take in widget objects directly

--- frontend/src/composables/useDraggables.ts
+++ frontend/src/composables/useDraggables.ts
@@ -12,7 +12,7 @@ import {
 	ResizeWidgetAction,
 } from '@/utils/actions'
 import { useSessionStore } from '@/stores/useSessionStore'
-import { ExpressionData, GraphData } from '@/utils/widgetData'
+import { ExpressionData, GraphData, type Widget } from '@/utils/widgetData'
 
 // PS eventually thsi may need to track pointerID for multi touch in the future PS
 // Description: computes a dx and dy for a pointer gesture, giving lifecycle hooks for it's movement
@@ -89,10 +89,9 @@ export function usePointerGestureCoordinateOffset(
 	return { start, isActive }
 }
 
-export function useWidgetDrag(id: string) {
+export function useWidgetDrag(widget: Widget) {
 	const widgetStore = useWidgetStore()
 	const sessionStore = useSessionStore()
-	const widget = widgetStore.getWidgetById(id)
 	const x = toRef(widget, 'x')
 	const y = toRef(widget, 'y')
 
@@ -101,7 +100,7 @@ export function useWidgetDrag(id: string) {
 
 	const { start, isActive: isDragging } = usePointerGestureCoordinateOffset(x, y, {
 		onDown: () => {
-			sessionStore.heldWidgetId = id
+			sessionStore.heldWidgetId = widget.id
 			startZIndex = widget.zIndex
 			newZIndex = widgetStore.bringWidgetToFrontSilently(widget)
 		},
@@ -142,9 +141,8 @@ export function useWidgetDrag(id: string) {
 	return { start, isDragging }
 }
 
-export function useWidgetResize(id: string) {
+export function useWidgetResize(widget: Widget) {
 	const widgetStore = useWidgetStore()
-	const widget = widgetStore.getWidgetById(id)
 	const width = toRef(widget, 'width')
 	const height = toRef(widget, 'height')
 
